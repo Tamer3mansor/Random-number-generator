cf6f12a83c0f818630e3e4bd8207316529db14c2
Tamer <62846154+Tamer3mansor@users.noreply.github.com>
2023-05-08 16:20:04 +0300

    last modification

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-// next step :> create generate all funcation to generate all dice random and but it on array to use in ejs 
+// next step :> create generate all funcation to generate all dice random and but it on array to use in ejs
 //
 
-var express = require('express');
-const app=express();
-var bodyParser = require('body-parser')
-let faces=0,number=0;
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let faces = 0,
+  number = 0;
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// use res.render to load up an ejs view file
-let generateAll=(faces,number)=>{
-number.array.forEach(element => {
-  Math.random()
-});
-}
+let generateAll = (number, faces) => {
+  let randoms = [];
+  for (let i = 0; i < number; i++) {
+    randoms[i] = Math.floor(Math.random() * faces);
+  }
+  return randoms;
+};
 // index page
-app.get('/', function(req, res) {
-  res.render('index' );
+app.get("/", function (req, res) {
+  res.render("index");
 });
-app.post('/res',(req,res)=>{
-  faces=req.body.faces || 0;
-  number=req.body.dices || 0;
-  res.render('respon' , {faces,type: number});
-  // res.send();
-})
-
-
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.post("/response", (req, res) => {
+  faces = req.body.faces || 0;
+  num_of_dices = req.body.num_of_dices || 0;
+  let randoms = [];
+  randoms = generateAll(num_of_dices, faces);
+  console.log(faces, num_of_dices, randoms);
+  res.render("response", { faces, num_of_dices, randoms });
+  res.send();
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log("Server is listening on port 8080");
